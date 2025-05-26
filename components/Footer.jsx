cf4878e06fa49e17aecab6b8ccc88af5f9a940e1
faizantanveeer <ftlegend007@gmail.com>
2025-05-26 09:33:52 +0500

    Footer fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    
-    <div className=' mx-auto text-center bg-green-50 '>
-      <p className='text-sm'>© 2025 Richloom. All rights reserved.</p>
-        
+    <div className=" mx-auto text-center bg-green-50 h-20 flex items-center justify-center gap-4 flex-col">
+      <p className="text-sm">
+        © 2025 Richloom. Created with ❤️ by Faizan Tanveer
+      </p>
     </div>
-    
-  )
+  );
 }
 
-export default Footer
+export default Footer;
