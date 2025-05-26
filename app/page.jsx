d933ff6e491dacd8c76e3b37b07cd434bc5c4411
faizantanveeer <ftlegend007@gmail.com>
2025-05-26 10:16:05 +0500

    Home page minor fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import HeroSection from "@/components/Hero";
 import { Card, CardContent } from "@/components/ui/card";
-import {Button} from "@/components/ui/button";
-import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
+import { Button } from "@/components/ui/button";
+import {
+  featuresData,
+  howItWorksData,
+  statsData,
+  testimonialsData,
+} from "@/data/landing";
 import Image from "next/image";
 import Link from "next/link";
-
 
 export default function Home() {
   return (
@@ -12,7 +16,7 @@ export default function Home() {
       <HeroSection />
 
       {/* Stats Section */}
-      <section className=" bg-green-50">
+      <section className=" bg-green-50 py-10">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 relative">
             {statsData.map((stat, index) => (
@@ -31,13 +35,17 @@ export default function Home() {
 
       <section className="py-20 bg-white">
         <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold text-center mb-12 ">All you need to manage your Finances</h2>
+          <h2 className="text-4xl font-bold text-center mb-12 ">
+            All you need to manage your Finances
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {featuresData.map((feature, index) => (
               <Card className="p-6 cursor-pointer hover:shadow-md" key={index}>
                 <CardContent className="space-y-4 pt-4">
                   {feature.icon}
-                  <h3 className="text-green-600 font-bold text-2xl">{feature.title}</h3>
+                  <h3 className="text-green-600 font-bold text-2xl">
+                    {feature.title}
+                  </h3>
                   <p className="text-gray-600">{feature.description}</p>
                 </CardContent>
               </Card>
@@ -46,18 +54,26 @@ export default function Home() {
         </div>
       </section>
 
-
       {/* How It Works Section */}
 
       <section className="py-20 bg-green-50">
         <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold text-center mb-16 ">How Richloom works?</h2>
+          <h2 className="text-4xl font-bold text-center mb-16 ">
+            How Richloom works?
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {howItWorksData.map((step, index) => (
-              <div key={index} className="items-center text-center p-6  flex flex-col gap-6">
-                <div className="w-16 h-16 bg-green-100 rounded-full mx-auto flex justify-center items-center">{step.icon}</div>
-                <h3 className="text-green-600 font-bold text-2xl">{step.title}</h3>
-                  <p className="text-gray-600">{step.description}</p>
+              <div
+                key={index}
+                className="items-center text-center p-6  flex flex-col gap-6"
+              >
+                <div className="w-16 h-16 bg-green-100 rounded-full mx-auto flex justify-center items-center">
+                  {step.icon}
+                </div>
+                <h3 className="text-green-600 font-bold text-2xl">
+                  {step.title}
+                </h3>
+                <p className="text-gray-600">{step.description}</p>
               </div>
             ))}
           </div>
@@ -68,27 +84,31 @@ export default function Home() {
 
       <section className="py-20 bg-white">
         <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold text-center mb-12 ">What our users say about Richloom?</h2>
+          <h2 className="text-4xl font-bold text-center mb-12 ">
+            What our users say about Richloom?
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {testimonialsData.map((testimonial, index) => (
               <Card className="p-6 cursor-pointer hover:shadow-md" key={index}>
                 <CardContent className="space-y-4 pt-4">
-                  
-                    <div className="flex items-center gap-4">
-
-                    <Image src={testimonial.image}
-                    alt={testimonial.name}
-                    width={40}
-                    className={"rounded-full "}
-                    height={40}/>
+                  <div className="flex items-center gap-4">
+                    <Image
+                      src={testimonial.image}
+                      alt={testimonial.name}
+                      width={40}
+                      className={"rounded-full "}
+                      height={40}
+                    />
 
                     <div>
-                      <h3 className="text-green-600 font-seibold text-2xl">{testimonial.name}</h3>
+                      <h3 className="text-green-600 font-seibold text-2xl">
+                        {testimonial.name}
+                      </h3>
                       <p className="text-gray-600">{testimonial.role}</p>
                     </div>
-                    </div>
-                    
-                    <p className="mt-2">"{testimonial.quote}"</p>
+                  </div>
+
+                  <p className="mt-2">"{testimonial.quote}"</p>
                 </CardContent>
               </Card>
             ))}
@@ -96,14 +116,19 @@ export default function Home() {
         </div>
       </section>
 
-
-{      /* CTA Section */}
+      {/* CTA Section */}
       <section className="py-20 bg-green-50">
         <div className="container mx-auto px-4 text-center">
-          <h2 className="text-4xl font-bold mb-6">Ready to take control of your finances?</h2>
-          <p className="text-gray-600 mb-8">Join thousands of users who trust Richloom to manage their finances.</p>
+          <h2 className="text-4xl font-bold mb-6">
+            Ready to take control of your finances?
+          </h2>
+          <p className="text-gray-600 mb-8">
+            Join thousands of users who trust Richloom to manage their finances.
+          </p>
           <Link href="/dashboard">
-          <Button className="bg-green-600 text-white py-3 px-6 rounded-lg hover:bg-green-700 transition duration-300">Take a Free Trial</Button>
+            <Button className="bg-green-600 text-white py-3 px-6 rounded-lg hover:bg-green-700 transition duration-300">
+              Take a Free Trial
+            </Button>
           </Link>
         </div>
       </section>
